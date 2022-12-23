e4c6b937a3670b351e5b0f05476c35006f7a33a0
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2022-12-23 11:43:59 -0500

    Initial commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About Us" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>About Us</h1>
+
+
+	<p>
+		<strong>We are <a href="https://fullstacksvelte.dev">Full Stack Svelte</a>, website and app developers. Welcome to our home!</strong>. You can can learn more abou our team here or by visiting the blog and resource sections of this website. We hope to hear from you!
+	</p>
+
+	<pre>We are the Svelte Full Stack Dev Team. Welcome!</pre>
+
+	<p>
+		We are a Svelte Development team that builds full stack applications for mobile devices, tablets, and desktop screens. We are available for hire - just message us on our <a href="/contact">contact us page</a>. We have plenty of information and other awesome resources that you can check out by browsing our website. Feel free to chip in on our project! We are open source and are on Discord and <a href="https://github.com/geauxweisbeck4">GitHub</a>.
+	</p>
+</div>
